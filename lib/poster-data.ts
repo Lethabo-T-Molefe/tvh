cc5b832c436d946890b6416efa255c4596fb0bfe
Lethabo-T-Molefe <lethabomolefe69@gmail.com>
2025-06-04 07:18:00 +0200

    Added poster section with carousel

---
+++ lib/poster-data.ts
@@ -0,0 +1,26 @@
+export const posterList = [
+  {
+    id: 1,
+    title: "TVH 2025 Main Poster",
+    description: "Join us for the TSHWANE VARSITY HACKATHON 2025, where innovation meets education! This year's hackathon promises to be bigger and better than ever!",
+    imageUrl:
+      "/assets/img/Posts/tvh2025.jpg",
+    category: "Main Event",
+  },
+  {
+    id: 2,
+    title: "TVH 2025 Launch Poster",
+    description: "Through the University Hackathon Series and in collaboration with the City of Tshwane, we are excited to announce the launch of TVH 2025!",
+    imageUrl:
+      "/assets/img/Posts/tvh-launch2025.png",
+    category: "Challenge",
+  },
+  {
+    id: 3,
+    title: "TVH Design Thinking",
+    description: "Join us for a workshop on Design Thinking, where you'll learn how to approach problem-solving with a user-centered mindset.",
+    imageUrl:
+      "/assets/img/Posts/tvhdesignthinking2025.jpg",
+    category: "Workshop",
+  },
+];
