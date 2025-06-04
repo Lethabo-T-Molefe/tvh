7bdb63459b560cb2b967c429cc04abaa7ba95d89
Lethabo-T-Molefe <lethabomolefe69@gmail.com>
2025-06-04 23:42:15 +0200

    g

--- components/sections/themes.tsx
+++ components/sections/themes.tsx
@@ -328,9 +328,7 @@ export default function Themes() {
               </p>
             </CardContent>
           </Card>
-        </motion.div>
-
-        
+        </motion.div>        
       </div>
     </section>
   );
